88a2ed7717b9ce98de8a6bd2da1604523d5be011
William Swartzendruber <wswartzendruber@gmail.com>
2022-09-26 20:05:42 -0600

    Basic ODS structure in place.

---
+++ PGS4NET/FinalObjectDefinitionSegment.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Copyright 2022 William Swartzendruber
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a
+ * copy of the MPL was not distributed with this file, You can obtain one at
+ * https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+namespace PGS4NET;
+
+public class FinalObjectDefinitionSegment : Segment
+{
+}
