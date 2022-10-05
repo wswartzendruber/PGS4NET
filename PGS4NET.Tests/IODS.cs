33c0d2e6dcb70c9fcfefdcb9d2946b2a7bfedf07
William Swartzendruber <wswartzendruber@gmail.com>
2022-10-05 16:59:17 -0600

    Adding I-ODS unit tests.

---
+++ PGS4NET.Tests/IODS.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Copyright 2022 William Swartzendruber
+ *
+ * To the extent possible under law, the person who associated CC0 with this file has waived all
+ * copyright and related or neighboring rights to this file.
+ *
+ * You should have received a copy of the CC0 legalcode along with this work. If not, see
+ * <http://creativecommons.org/publicdomain/zero/1.0/>.
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+namespace PGS4NET.Tests;
+
+using PGS4NET;
+
+internal static class IODS
+{
+    internal static byte[] Empty = new byte[]
+    {
+        0x50, 0x47,             // Magic number
+        0x01, 0x23, 0x45, 0x67, // PTS
+        0x12, 0x34, 0x56, 0x78, // DTS
+        0x15,                   // Type
+        0x00, 0x0B,             // Size
+        0xA0, 0xA1,             // ID
+        0xA2,                   // Version
+        0x80,                   // Sequence flags
+        0xAB, 0xCD, 0xEF,       // Data length
+        0x21, 0x43,             // Width
+        0x65, 0x87,             // Height
+    };
+
+    internal static byte[] Small = new byte[]
+    {
+        0x50, 0x47,             // Magic number
+        0x01, 0x23, 0x45, 0x67, // PTS
+        0x12, 0x34, 0x56, 0x78, // DTS
+        0x15,                   // Type
+        0x00, 0x0F,             // Size
+        0xA0, 0xA1,             // ID
+        0xA2,                   // Version
+        0x80,                   // Sequence flags
+        0xAB, 0xCD, 0xEF,       // Data length
+        0x21, 0x43,             // Width
+        0x65, 0x87,             // Height
+        0xE0, 0xE1, 0xE2, 0xE3, // Data
+    };
+}
---
+++ PGS4NET.Tests/IODSReadTests.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * Copyright 2022 William Swartzendruber
+ *
+ * To the extent possible under law, the person who associated CC0 with this file has waived all
+ * copyright and related or neighboring rights to this file.
+ *
+ * You should have received a copy of the CC0 legalcode along with this work. If not, see
+ * <http://creativecommons.org/publicdomain/zero/1.0/>.
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+namespace PGS4NET.Tests;
+
+using System.IO;
+using PGS4NET;
+
+public class IODSReadTests
+{
+    [Fact]
+    public void Empty()
+    {
+        using (var stream = new MemoryStream(IODS.Empty))
+        {
+            var segment = stream.ReadSegment();
+
+            Assert.True(segment.PTS == 0x01234567);
+            Assert.True(segment.DTS == 0x12345678);
+
+            if (segment is InitialObjectDefinitionSegment sods)
+            {
+                Assert.True(sods.ID == 0xA0A1);
+                Assert.True(sods.Version == 0xA2);
+                Assert.True(sods.Length == 0xABCDEF);
+                Assert.True(sods.Width == 0x2143);
+                Assert.True(sods.Height == 0x6587);
+                Assert.True(sods.Data.Length == 0);
+            }
+            else
+            {
+                Assert.True(false);
+            }
+        }
+    }
+
+    [Fact]
+    public void Small()
+    {
+        using (var stream = new MemoryStream(IODS.Small))
+        {
+            var segment = stream.ReadSegment();
+
+            Assert.True(segment.PTS == 0x01234567);
+            Assert.True(segment.DTS == 0x12345678);
+
+            if (segment is InitialObjectDefinitionSegment sods)
+            {
+                Assert.True(sods.ID == 0xA0A1);
+                Assert.True(sods.Version == 0xA2);
+                Assert.True(sods.Length == 0xABCDEF);
+                Assert.True(sods.Width == 0x2143);
+                Assert.True(sods.Height == 0x6587);
+                Assert.True(sods.Data.Length == 4);
+                Assert.True(sods.Data[0] == 0xE0);
+                Assert.True(sods.Data[1] == 0xE1);
+                Assert.True(sods.Data[2] == 0xE2);
+                Assert.True(sods.Data[3] == 0xE3);
+            }
+            else
+            {
+                Assert.True(false);
+            }
+        }
+    }
+}
