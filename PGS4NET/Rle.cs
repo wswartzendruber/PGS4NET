eabbb5a130a835a056d1c5f38040183c56f55d5a
William Swartzendruber <wswartzendruber@gmail.com>
2023-07-10 16:59:17 -0600

    Adding (tenatively complete) RLE compression support.

--- PGS4NET/Rle.cs
+++ PGS4NET/Rle.cs
@@ -17,15 +17,63 @@ namespace PGS4NET;
 /// </summary>
 public static class Rle
 {
+    // COMPRESSION
+    private static RleException LineTooLong = new("Line too long.");
+
+    // DECOMPRESSION
     private static RleException IncompleteRleLine = new("Incomplete RLE line.");
     private static RleException IncompleteRleSequence = new("Incomplete RLE sequence.");
     private static RleException InvalidRleSequence = new("Invalid RLE sequence.");
 
     /// <summary>
+    ///     Compresses the provided ordered list of lines into a byte sequence.
+    /// </summary>
+    /// <param name="input">
+    ///     An ordered list of lines, each one of which contains an ordered list of palette
+    ///     entries defined elsewhere.
+    /// </param>
+    /// <returns>
+    ///     An RLE-compressed byte sequence.
+    /// </returns>
+    public static IList<byte> Compress(IList<IList<byte>> input)
+    {
+        var output = new List<byte>();
+        byte value = 0x00;
+        uint count = 0;
+
+        foreach (var line in input)
+        {
+            foreach (var nextValue in line)
+            {
+                if (nextValue == value)
+                {
+                    count += 1;
+                }
+                else
+                {
+                    if (count > 0)
+                        OutputRleSequence(output, value, count);
+                    value = nextValue;
+                    count = 1;
+                }
+            }
+
+            OutputRleSequence(output, value, count);
+            value = 0;
+            count = 0;
+
+            output.Add(0x00);
+            output.Add(0x00);
+        }
+
+        return output;
+    }
+
+    /// <summary>
     ///     Decompresses the provided byte sequence into an ordered list of lines.
     /// </summary>
     /// <param name="input">
-    ///     The RLE-compressed byte sequence to decompress.
+    ///     An RLE-compressed byte sequence.
     /// </param>
     /// <returns>
     ///     An ordered list of lines, each one of which contains an ordered list of palette
@@ -145,5 +193,56 @@ public static class Rle
             throw IncompleteRleLine;
 
         return output;
+    }
+
+    private static void OutputRleSequence(List<byte> output, byte value, uint count)
+    {
+        if (value == 0x00)
+        {
+            switch (count)
+            {
+                case 0:
+                    break;
+                case < 64:
+                    output.Add(0x00);
+                    output.Add((byte)count);
+                    break;
+                case < 16_384:
+                    output.Add(0x00);
+                    output.Add((byte)(0x40 | count >> 8));
+                    output.Add((byte)(count & 0xFF));
+                    break;
+                default:
+                    throw LineTooLong;
+            }
+        }
+        else
+        {
+            switch (count)
+            {
+                case 0:
+                    break;
+                case 1:
+                    output.Add(value);
+                    break;
+                case 2:
+                    output.Add(value);
+                    output.Add(value);
+                    break;
+                case < 64:
+                    output.Add(0x00);
+                    output.Add((byte)(0x80 | count));
+                    output.Add(value);
+                    break;
+                case < 16_384:
+                    output.Add(0x00);
+                    output.Add((byte)(0xC0 | count >> 8));
+                    output.Add((byte)(count & 0xFF));
+                    output.Add(value);
+                    break;
+                default:
+                    throw LineTooLong;
+            }
+        }
     }
 }
