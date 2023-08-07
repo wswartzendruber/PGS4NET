fb5753f4a8362cf809e9c5a5626170997b0d0201
William Swartzendruber <wswartzendruber@gmail.com>
2023-08-07 17:26:54 -0600

    Testing IDisposable and IAsyncDisposable for SegmentReader and SegmentWriter.

---
+++ PGS4NET.Tests/DisposalTests.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * Copyright 2023 William Swartzendruber
+ *
+ * To the extent possible under law, the person who associated CC0 with this file has waived all
+ * copyright and related or neighboring rights to this file.
+ *
+ * You should have received a copy of the CC0 legalcode along with this work. If not, see
+ * <http://creativecommons.org/publicdomain/zero/1.0/>.
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+using PGS4NET.Segments;
+
+namespace PGS4NET.Tests;
+
+public class DisposalTests
+{
+    [Fact]
+    public void SegmentReader_Using()
+    {
+        var testStream = new TestStream();
+
+        Assert.False(testStream.Disposed);
+
+        using (var reader = new SegmentReader(testStream))
+        {
+            Assert.False(testStream.Disposed);
+        }
+
+        Assert.True(testStream.Disposed);
+    }
+
+#if TEST_NETSTANDARD2_1
+    [Fact]
+    public async Task SegmentReader_AwaitUsing()
+    {
+        var testStream = new TestStream();
+
+        Assert.False(testStream.Disposed);
+
+        await using (var reader = new SegmentReader(testStream))
+        {
+            Assert.False(testStream.Disposed);
+        }
+
+        Assert.True(testStream.Disposed);
+    }
+#endif
+
+    [Fact]
+    public void SegmentWriter_Using()
+    {
+        var testStream = new TestStream();
+
+        Assert.False(testStream.Disposed);
+
+        using (var writer = new SegmentWriter(testStream))
+        {
+            Assert.False(testStream.Disposed);
+        }
+
+        Assert.True(testStream.Disposed);
+    }
+
+#if TEST_NETSTANDARD2_1
+    [Fact]
+    public async Task SegmentWriter_AwaitUsing()
+    {
+        var testStream = new TestStream();
+
+        Assert.False(testStream.Disposed);
+
+        await using (var writer = new SegmentWriter(testStream))
+        {
+            Assert.False(testStream.Disposed);
+        }
+
+        Assert.True(testStream.Disposed);
+    }
+#endif
+}
---
+++ PGS4NET.Tests/TestStream.cs
@@ -0,0 +1,103 @@
+﻿/*
+ * Copyright 2023 William Swartzendruber
+ *
+ * To the extent possible under law, the person who associated CC0 with this file has waived all
+ * copyright and related or neighboring rights to this file.
+ *
+ * You should have received a copy of the CC0 legalcode along with this work. If not, see
+ * <http://creativecommons.org/publicdomain/zero/1.0/>.
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+namespace PGS4NET.Tests;
+
+internal class TestStream : Stream
+{
+    public bool Disposed = false;
+
+    public override bool CanRead
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public override bool CanSeek
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public override bool CanWrite
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public override void Flush()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override long Length
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public override long Position
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+        set
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void SetLength(long value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override async ValueTask DisposeAsync()
+    {
+        Disposed = true;
+
+        await base.DisposeAsync();
+
+        GC.SuppressFinalize(this);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        Disposed = true;
+
+        base.Dispose(disposing);
+    }
+}
