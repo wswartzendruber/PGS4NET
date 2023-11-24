7ebe2594a8d35ad85c872585f31628aaec0ee918
William Swartzendruber <wswartzendruber@gmail.com>
2023-11-24 10:48:28 -0800

    Beginnings of Caption composition.

--- PGS4NET/Captions/CaptionComposer.cs
+++ PGS4NET/Captions/CaptionComposer.cs
@@ -16,22 +16,43 @@ namespace PGS4NET.Captions;
 
 /// <summary>
 ///     Statefully composes captions using sequentially input display sets. Also supports
-///     immediate caption decomposition into display sets.
+///     caption decomposition into display sets.
 /// </summary>
 public class CaptionComposer
 {
+    private readonly Dictionary<byte, DisplayWindow> Windows = new();
+    private readonly Dictionary<byte, DisplayPalette> Palettes = new();
+    private readonly Dictionary<ushort, DisplayObject> Objects = new();
+
     /// <summary>
-    ///     Inputs the next <see cref="DisplaySet" /> into the composer, returning a new
-    ///     <see cref="Caption" /> instance if one can be composed, or <see langword="null" />
-    ///     if more display sets are required.
+    ///     Inputs the next <see cref="DisplaySet" /> into the composer, returning a collection
+    ///     of any new <see cref="Caption" />s that could be composed, or an empty collection
+    ///     if none could be composed from the provided <see cref="DisplaySet" /> alone.
     /// </summary>
     /// <exception cref="CaptionException">
-    ///     Thrown when a combination of otherwise valid display sets cannot be combined into a
-    ///     caption.
+    ///     Thrown when a combination of otherwise valid display sets are invalid when taken
+    ///     together.
     /// </exception>
-    public Caption? Input(DisplaySet displaySet)
+    public IList<Caption> Input(DisplaySet displaySet)
     {
-        return null;
+        var returnValue = new List<Caption>();
+
+        if (displaySet.CompositionState == CompositionState.EpochStart)
+            Reset();
+
+        foreach (var window in displaySet.Windows)
+            Windows[window.Key] = window.Value;
+        foreach (var palette in displaySet.Palettes)
+            Palettes[palette.Key.Id] = palette.Value;
+        foreach (var object_ in displaySet.Objects)
+            Objects[object_.Key.Id] = object_.Value;
+
+        foreach (var composition in displaySet.Compositions)
+        {
+            // TODO: RESUME HERE
+        }
+
+        return returnValue;
     }
 
     /// <summary>
@@ -39,6 +60,9 @@ public class CaptionComposer
     /// </summary>
     public void Reset()
     {
+        Windows.Clear();
+        Palettes.Clear();
+        Objects.Clear();
     }
 
     /// <summary>
