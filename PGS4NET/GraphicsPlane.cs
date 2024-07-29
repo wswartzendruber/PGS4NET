38f6de01a5e689ea70ad09b87c62b02297cd4b6c
William Swartzendruber <wswartzendruber@gmail.com>
2024-07-28 21:55:08 -0600

    More work on the GraphicsPlane compositor.

---
+++ PGS4NET/GraphicsPlane.cs
@@ -0,0 +1,123 @@
+﻿/*
+ * Copyright 2024 William Swartzendruber
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a
+ * copy of the MPL was not distributed with this file, You can obtain one at
+ * https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+using System;
+using PGS4NET.Captions;
+using PGS4NET.DisplaySets;
+
+namespace PGS4NET;
+
+public class GraphicsPlane
+{
+    private readonly uint Size;
+
+    private PgsPixel[] PrimaryPixels;
+    private PgsPixel[] SecondaryPixels;
+    private PgsTimeStamp LastTimeStamp = new PgsTimeStamp(0);
+    private bool LastForced = false;
+
+    public event EventHandler<Caption>? NewCaptionReady;
+
+    public ushort X { get; }
+
+    public ushort Y { get; }
+
+    public ushort Width { get; }
+
+    public ushort Height { get; }
+
+    public GraphicsPlane(DisplayWindow displayWindow)
+    {
+        Size = (uint)(displayWindow.Width * displayWindow.Height);
+
+        PrimaryPixels = new PgsPixel[Size];
+        SecondaryPixels = new PgsPixel[Size];
+
+        X = displayWindow.X;
+        Y = displayWindow.Y;
+        Width = displayWindow.Width;
+        Height = displayWindow.Height;
+    }
+
+    public void Clear(PgsTimeStamp timeStamp)
+    {
+        SwapPlanes();
+
+        for (uint i = 0; i < Size; i++)
+            PrimaryPixels[i] = default;
+
+        if (PlanesDiffer())
+        {
+            var pixels = new PgsPixel[Size]; Array.Copy(PrimaryPixels, pixels, Size);
+            var caption = new Caption(LastTimeStamp, timeStamp - LastTimeStamp, X, Y, Width
+                , Height, pixels, LastForced);
+
+            OnNewCaptionReady(caption);
+        }
+
+        LastTimeStamp = timeStamp;
+        LastForced = false;
+    }
+
+    public void Draw(PgsTimeStamp timeStamp, DisplayObject displayObject
+        , DisplayComposition displayComposition, DisplayPalette displayPalette)
+    {
+        int x, y, width, height;
+
+        if (displayComposition.Crop is Crop crop)
+        {
+            x = displayComposition.X - X + crop.X;
+            y = displayComposition.Y - Y + crop.Y;
+            width = crop.Width;
+            height = crop.Height;
+        }
+        else
+        {
+            x = displayComposition.X - X;
+            y = displayComposition.Y - Y;
+            width = displayObject.Width;
+            height = displayObject.Height;
+        }
+    }
+
+    public void Reset()
+    {
+        for (uint i = 0; i < Size; i++)
+        {
+            PrimaryPixels[i] = default;
+            SecondaryPixels[i] = default;
+        }
+    }
+
+    private void OnNewCaptionReady(Caption caption)
+    {
+        if (NewCaptionReady is EventHandler<Caption> handler)
+            handler(this, caption);
+    }
+
+    private bool PlanesDiffer()
+    {
+        for (uint i = 0; i < Size; i++)
+        {
+            if (PrimaryPixels[i] != SecondaryPixels[i])
+                return false;
+        }
+
+        return true;
+    }
+
+    private void SwapPlanes()
+    {
+        var temp = PrimaryPixels;
+
+        PrimaryPixels = SecondaryPixels;
+        SecondaryPixels = temp;
+    }
+}
