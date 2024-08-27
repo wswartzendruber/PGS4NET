2b064d80f5c6536e98bea25e7fd380d15a552ff8
William Swartzendruber <wswartzendruber@gmail.com>
2024-08-26 20:01:17 -0600

    More Compositor unit tests relating to valid cropping and positioning.

--- PGS4NET.Tests/CompositorTests.cs
+++ PGS4NET.Tests/CompositorTests.cs
@@ -18,7 +18,8 @@ namespace PGS4NET.Tests;
 
 public class CompositorTests
 {
-    private static readonly PgsPixel Pixel = new PgsPixel(0xFF, 0xEE, 0xDD, 0xCC);
+    private static readonly PgsPixel Pixel1 = new PgsPixel(0xFF, 0xEE, 0xDD, 0xCC);
+    private static readonly PgsPixel Pixel2 = new PgsPixel(0xFF, 0xFF, 0xFF, 0xFF);
     private static readonly PgsTimeStamp TimeStamp1 = new PgsTimeStamp(8);
     private static readonly PgsTimeStamp TimeStamp2 = new PgsTimeStamp(16);
 
@@ -42,12 +43,12 @@ public class CompositorTests
     }
 
     [Fact]
-    public void Single()
+    public void FullWindowNoCrop()
     {
         var entries = new Dictionary<byte, PgsPixel>
         {
             { 0, default },
-            { 1, Pixel },
+            { 1, Pixel1 },
         };
         var data1 = new byte[]
         {
@@ -64,10 +65,162 @@ public class CompositorTests
         var captions = new List<Caption>();
         var expectedPixels1 = new PgsPixel[]
         {
-            Pixel, default, default, Pixel,
-            default, Pixel, Pixel, default,
-            default, Pixel, Pixel, default,
-            Pixel, default, default, Pixel,
+            Pixel1, default, default, Pixel1,
+            default, Pixel1, Pixel1, default,
+            default, Pixel1, Pixel1, default,
+            Pixel1, default, default, Pixel1,
+        };
+
+        compositor.NewCaptionReady += (sender, caption) =>
+        {
+            Assert.True(sender == compositor);
+
+            captions.Add(caption);
+        };
+
+        compositor.Draw(TimeStamp1, object1, composition, palette);
+        compositor.Clear(TimeStamp2);
+
+        Assert.True(captions.Count == 1);
+        Assert.True(captions[0].TimeStamp == TimeStamp1);
+        Assert.True(captions[0].Duration == TimeStamp2 - TimeStamp1);
+        Assert.True(captions[0].X == 8);
+        Assert.True(captions[0].Y == 16);
+        Assert.True(captions[0].Width == 4);
+        Assert.True(captions[0].Height == 4);
+        Assert.True(captions[0].Forced == false);
+        Assert.True(captions[0].Data.SequenceEqual(expectedPixels1));
+    }
+
+    [Fact]
+    public void FullWindowCrop()
+    {
+        var entries = new Dictionary<byte, PgsPixel>
+        {
+            { 0, default },
+            { 1, Pixel1 },
+            { 2, Pixel2 },
+        };
+        var data1 = new byte[]
+        {
+            2, 2, 2, 2,
+            2, 1, 0, 2,
+            2, 0, 1, 2,
+            2, 2, 2, 2,
+        };
+        var palette = new DisplayPalette(entries);
+        var object1 = new DisplayObject(4, 4, data1);
+        var window = new DisplayWindow(8, 16, 2, 2);
+        var composition = new DisplayComposition(8, 16, false, new Crop(1, 1, 2, 2));
+        var compositor = new Compositor(window);
+        var captions = new List<Caption>();
+        var expectedPixels1 = new PgsPixel[]
+        {
+            Pixel1, default,
+            default, Pixel1,
+        };
+
+        compositor.NewCaptionReady += (sender, caption) =>
+        {
+            Assert.True(sender == compositor);
+
+            captions.Add(caption);
+        };
+
+        compositor.Draw(TimeStamp1, object1, composition, palette);
+        compositor.Clear(TimeStamp2);
+
+        Assert.True(captions.Count == 1);
+        Assert.True(captions[0].TimeStamp == TimeStamp1);
+        Assert.True(captions[0].Duration == TimeStamp2 - TimeStamp1);
+        Assert.True(captions[0].X == 8);
+        Assert.True(captions[0].Y == 16);
+        Assert.True(captions[0].Width == 2);
+        Assert.True(captions[0].Height == 2);
+        Assert.True(captions[0].Forced == false);
+        Assert.True(captions[0].Data.SequenceEqual(expectedPixels1));
+    }
+
+    [Fact]
+    public void PartialWindowNoCrop()
+    {
+        var entries = new Dictionary<byte, PgsPixel>
+        {
+            { 0, default },
+            { 1, Pixel1 },
+        };
+        var data1 = new byte[]
+        {
+            1, 0, 0, 1,
+            0, 1, 1, 0,
+            0, 1, 1, 0,
+            1, 0, 0, 1,
+        };
+        var palette = new DisplayPalette(entries);
+        var object1 = new DisplayObject(4, 4, data1);
+        var window = new DisplayWindow(8, 16, 6, 6);
+        var composition = new DisplayComposition(9, 17, false, null);
+        var compositor = new Compositor(window);
+        var captions = new List<Caption>();
+        var expectedPixels1 = new PgsPixel[]
+        {
+            default, default, default, default, default, default,
+            default, Pixel1, default, default, Pixel1, default,
+            default, default, Pixel1, Pixel1, default, default,
+            default, default, Pixel1, Pixel1, default, default,
+            default, Pixel1, default, default, Pixel1, default,
+            default, default, default, default, default, default,
+        };
+
+        compositor.NewCaptionReady += (sender, caption) =>
+        {
+            Assert.True(sender == compositor);
+
+            captions.Add(caption);
+        };
+
+        compositor.Draw(TimeStamp1, object1, composition, palette);
+        compositor.Clear(TimeStamp2);
+
+        Assert.True(captions.Count == 1);
+        Assert.True(captions[0].TimeStamp == TimeStamp1);
+        Assert.True(captions[0].Duration == TimeStamp2 - TimeStamp1);
+        Assert.True(captions[0].X == 8);
+        Assert.True(captions[0].Y == 16);
+        Assert.True(captions[0].Width == 6);
+        Assert.True(captions[0].Height == 6);
+        Assert.True(captions[0].Forced == false);
+        Assert.True(captions[0].Data.SequenceEqual(expectedPixels1));
+    }
+
+    [Fact]
+    public void PartialWindowCrop()
+    {
+        var entries = new Dictionary<byte, PgsPixel>
+        {
+            { 0, default },
+            { 1, Pixel1 },
+            { 2, Pixel2 },
+        };
+        var data1 = new byte[]
+        {
+            2, 2, 2, 2,
+            2, 1, 1, 2,
+            2, 1, 1, 2,
+            2, 2, 2, 2,
+        };
+        var palette = new DisplayPalette(entries);
+        var object1 = new DisplayObject(4, 4, data1);
+        var window = new DisplayWindow(8, 16, 4, 4);
+        var composition = new DisplayComposition(9, 17, false, new Crop(1, 1, 2, 2));
+        var compositor = new Compositor(window);
+        var captions = new List<Caption>();
+        var expectedPixels1 = new PgsPixel[]
+        {
+            default, default, default, default,
+            default, Pixel1, Pixel1, default,
+            default, Pixel1, Pixel1, default,
+            default, default, default, default,
         };
 
         compositor.NewCaptionReady += (sender, caption) =>
