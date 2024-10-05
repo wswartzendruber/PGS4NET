6aa761d921ca2bcd06c4ac69efdc9a5e3f262cfa
William Swartzendruber <wswartzendruber@gmail.com>
2024-10-05 17:06:47 -0600

    Broken pixel conversion.

---
+++ PGS4NET.Tests/PixelConversionTests.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Copyright 2024 William Swartzendruber
+ *
+ * To the extent possible under law, the person who associated CC0 with this file has waived all
+ * copyright and related or neighboring rights to this file.
+ *
+ * You should have received a copy of the CC0 legalcode along with this work. If not, see
+ * <http://creativecommons.org/publicdomain/zero/1.0/>.
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#pragma warning disable 1718
+
+using PGS4NET;
+
+namespace PGS4NET.Tests;
+
+public class PixelConversionTests
+{
+    [Fact]
+    public void RoundTripBt709()
+    {
+        var colorSpace = ColorSpace.Bt709ColorSpace;
+
+        for (byte y = 16; y < 235; y++)
+        {
+            for (short cb = 0; cb <= 255; cb++)
+            {
+                for (short cr = 0; cr <= 255; cr++)
+                {
+                    for (short alpha = 0; alpha <= 255; alpha++)
+                    {
+                        var inPixel = new YcbcraPixel(y, (byte)cb, (byte)cr, (byte)alpha);
+                        var outPixel = colorSpace.RgbaToYcbcra(colorSpace.YcbcraToRgba(inPixel));
+
+                        Console.Error.WriteLine("==========================");
+                        Console.Error.WriteLine($"IN_PIXEL: {inPixel}");
+                        Console.Error.WriteLine($"OUT_PIXEL: {outPixel}");
+
+                        Assert.True(inPixel == outPixel);
+                    }
+                }
+            }
+        }
+    }
+}
--- PGS4NET/ColorSpace.cs
+++ PGS4NET/ColorSpace.cs
@@ -15,7 +15,7 @@ namespace PGS4NET;
 /// <summary>
 ///     Represents a RGB pixel.
 /// </summary>
-public struct ColorSpace : IEquatable<ColorSpace>
+public class ColorSpace : IEquatable<ColorSpace>
 {
     /// <summary>
     ///     Preconfigured RGB coefficients for BT.709.
@@ -51,6 +51,15 @@ public struct ColorSpace : IEquatable<ColorSpace>
     /// </summary>
     public readonly double Blue;
 
+    private readonly double Fr3;
+    private readonly double Fg2;
+    private readonly double Fg3;
+    private readonly double Fb2;
+    private readonly double Rb1;
+    private readonly double Rb2;
+    private readonly double Rr2;
+    private readonly double Rr3;
+
     /// <summary>
     ///     Creates a new instance with the provided color coefficients.
     /// </summary>
@@ -68,6 +77,14 @@ public struct ColorSpace : IEquatable<ColorSpace>
         Red = red;
         Green = green;
         Blue = blue;
+        Fr3 = 2.0 - 2.0 * red;
+        Fg2 = -(blue / green) * (2.0 - 2.0 * blue);
+        Fg3 = -(red / green) * (2.0 - 2.0 * red);
+        Fb2 = 2.0 - 2.0 * blue;
+        Rb1 = -0.5 * (Red / (1.0 - Blue));
+        Rb2 = -0.5 * (Green / (1.0 - Blue));
+        Rr2 = -0.5 * (Green / (1.0 - Red));
+        Rr3 = -0.5 * (Blue / (1.0 - Red));
     }
 
     /// <summary>
@@ -101,6 +118,37 @@ public struct ColorSpace : IEquatable<ColorSpace>
         }
     }
 
+    public YcbcraPixel RgbaToYcbcra(RgbaPixel rgbaPixel)
+    {
+        var y1 = Red * rgbaPixel.Red + Green * rgbaPixel.Green + Blue * rgbaPixel.Blue;
+        var y2 = (Compress(y1) * 255.0) - 0.25;
+        var y3 = Math.Round(Math.Min(Math.Max(y2, 0.0), 255.0));
+        var cb1 = Rb1 * rgbaPixel.Red + Rb2 * rgbaPixel.Green + 0.5 * rgbaPixel.Blue;
+        var cb2 = cb1 * 128.0;
+        var cb3 = Math.Round(Math.Min(Math.Max(cb2, 0.0), 255.0));
+        var cr1 = 0.5 * rgbaPixel.Red + Rr2 * rgbaPixel.Green + Rr3 * rgbaPixel.Blue;
+        var cr2 = cr1 * 128.0;
+        var cr3 = Math.Round(Math.Min(Math.Max(cr2, 0.0), 255.0));
+        var alpha1 = (rgbaPixel.Alpha) * 255.0;
+        var alpha2 = Math.Round(Math.Min(Math.Max(alpha1, 0.0), 255.0));
+
+        return new YcbcraPixel((byte)y3, (byte)cb3, (byte)cr3, (byte)alpha2);
+    }
+
+    public RgbaPixel YcbcraToRgba(YcbcraPixel ycbcraPixel)
+    {
+        var y = Expand((double)ycbcraPixel.Y / 255.0);
+        var pb = ((double)ycbcraPixel.Cb - 128.0) / 128.0;
+        var pr = ((double)ycbcraPixel.Cr - 128.0) / 128.0;
+        var alpha = (double)ycbcraPixel.Alpha / 255.0;
+
+        var red = 1.0 + Fr3 * pr;
+        var green = 1.0 + Fg2 * pb + Fg3 * pr;
+        var blue = 1.0 + Fb2 * pb;
+
+        return new RgbaPixel(red, green, blue, alpha);
+    }
+
     /// <summary>
     ///     Determines if the values of two <see cref="ColorSpace" />s match each other.
     /// </summary>
@@ -113,4 +161,19 @@ public struct ColorSpace : IEquatable<ColorSpace>
     /// </summary>
     public static bool operator !=(ColorSpace first, ColorSpace second) =>
         !first.Equals(second);
+
+    private double Compress(double value)
+    {
+        return (value * 0.859375) + 0.06274509803;
+    }
+
+    private double Expand(double value)
+    {
+        if (value < 0.06274509803)
+            return 0.0;
+        else if (value > 0.92156862745)
+            return 1.0;
+        else
+            return (value - 0.06274509803) / 0.859375;
+    }
 }
