5d0302630208e13995913c2c7daa2452bd48789d
William Swartzendruber <wswartzendruber@gmail.com>
2025-07-12 12:33:36 -0600

    REUSE compliance.

--- PGS4NET/Captions/CompositionArea.cs
+++ PGS4NET/Captions/CompositionArea.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*
+ * Copyright 2025 William Swartzendruber
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a
+ * copy of the MPL was not distributed with this file, You can obtain one at
+ * https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+using System;
 using PGS4NET.DisplaySets;
 
 namespace PGS4NET.Captions;
--- PGS4NET/DisplaySets/StreamExtensions.cs
+++ PGS4NET/DisplaySets/StreamExtensions.cs
@@ -1,4 +1,14 @@
-﻿using System.Collections.Generic;
+﻿/*
+ * Copyright 2025 William Swartzendruber
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a
+ * copy of the MPL was not distributed with this file, You can obtain one at
+ * https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+using System.Collections.Generic;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Threading;
